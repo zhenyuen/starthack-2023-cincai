b480198e2f4acb15dd7d1fd4b4ba7f56bded0caa
zhenyuen <zhenyuen.dev@gmail.com>
2023-03-24 01:56:15 +0100

    feat: colors

--- src/static/script.js
+++ src/static/script.js
@@ -2,20 +2,6 @@
 
 
 window.addEventListener('load', async function () {
-    const div = document.getElementById("player-1");
-    const tooltip = document.getElementById("tooltip-1");
-
-
-    div.addEventListener("mouseover", function(event) {
-        // Show the tooltip
-        tooltip.style.display = "block";
-    });
-
-    div.addEventListener("mouseout", function() {
-        // Hide the tooltip
-        tooltip.style.display = "none";
-    });
-    
     const session_id = prompt("Please input session id: ")
     const chat = await get_chat(session_id)
     const users = await get_users(session_id)
@@ -44,10 +30,14 @@ window.addEventListener('load', async function () {
         c3.textContent = `${e.username}: ${e.message}`
         const c4 = this.document.createElement("div")
         c4.className = "chat-time"
+        const c5 = this.document.createElement("span")
+        c5.setAttribute("style", "visibility: hidden")
+        c5.textContent = 5
         c4.textContent = t
         c2.appendChild(c3)
         c1.appendChild(c2)
         c1.appendChild(c4)
+        c1.appendChild(c5)
         c0.appendChild(c1)
         node.appendChild(c0)
     }
@@ -55,9 +45,28 @@ window.addEventListener('load', async function () {
     const node2 = document.getElementById("people");
 
     for (let i = 0; i < users.length; i++) {
+        const c6 = this.document.createElement("div")
+        c6.classList.add("tooltiptext")
+
+        const c7 = this.document.createElement("p")
+        c7.textContent = "AI suggested score: 6.9"
+
+        const c8 = this.document.createElement("p")
+        c8.textContent = "AI suggested score: 6.9"
+        const c9 = this.document.createElement("p")
+        c9.textContent = "AI suggested score: 6.9"
+        const c10 = this.document.createElement("p")
+        c10.textContent = "AI suggested score: 6.9"
+        c6.appendChild(c7)
+        c6.appendChild(c8)
+        c6.appendChild(c9)
+        c6.appendChild(c10)
+
         const c0 = document.createElement("li")
         const c1 = document.createElement("div")
-        c1.className = "player-container"
+        c1.classList.add("player-container")
+        c1.classList.add("tooltip")
+
         const c2 = document.createElement("span")
         c2.className = "player"
         c2.textContent = users[i].username
@@ -84,10 +93,35 @@ window.addEventListener('load', async function () {
         c3.appendChild(c44)
         c1.appendChild(c2)
         c1.appendChild(c3)
+        c1.appendChild(c6)
         c0.appendChild(c1)
         node2.appendChild(c0)
+
+
+        // c2.addEventListener("mouseover", function(event) {
+        //     // Show the tooltip
+        //     c1.style.display = "block";
+        // });
+
+        // c2.addEventListener("mouseout", function() {
+        //     // Hide the tooltip
+        //     c1.style.display = "none";
+        // });
+
     }
 
+    const chat_messages = document.getElementsByClassName("chat-container");
+
+    for (let i = 0; i < chat_messages.length; i++) {
+        var current = chat_messages[i]; // replace "myDiv" with the ID of your div
+        var value = Number(chat_messages[i].lastElementChild.innerHTML)
+        value = (value + 2) / 0.9
+        var g = 255 - (value - 1) * 25.5; // decrease red value as value increases
+        var r = (value - 1) * 25.5; // increase green value as value increases
+        var b = 0; // set blue value to 0
+        current.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
+        console.log(current)
+}
 
 
 });
