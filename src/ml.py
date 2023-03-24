8df10132230fa7ae7fcbccf6cf29830d24b88d95
zhenyuen <zhenyuen.dev@gmail.com>
2023-03-24 02:39:38 +0100

    add cluster

---
+++ src/ml.py
@@ -0,0 +1,16 @@
+import pickle
+import numpy as np
+
+
+def get_cluster(features):
+    with open("gmm", "rb") as f:
+        gmm = pickle.load(f)
+    
+    probs = gmm.predict_proba(features)
+    max_indices = np.argmax(probs, axis=1)
+    vector = np.zeros((probs.shape[0],), dtype=np.int64)
+    vector[np.arange(probs.shape[0])] = max_indices
+
+    return vector
+
+    
